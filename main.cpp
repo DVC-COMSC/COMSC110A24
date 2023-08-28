90256ad101ba1a0696641ddbb114dbab44cafbd9
Kyuwoong Lee <kwooler@gmail.com>
2023-08-27 17:28:12 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  double regular_charge = 59.95;
+  double discount_amount;
+  double sales_price;
+  double total;
+
+  /* ************************************
+    Code your program here
+  ************************************ */
+
+  cout << setprecision(2) << fixed;
+  cout << "Regular price: $" << regular_charge << endl;
+  cout << "Discount amount: $" << discount_amount << endl;
+  cout << "Sale price: $" << sales_price << endl;
+}
